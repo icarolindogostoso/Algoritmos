1555337313793bdf646f417c146d9fa249445034
Icaro Gabriel <icarogabrielpc05@gmail.com>
2024-11-11 21:46:26 -0300

    mensagem

--- extra/a.c
+++ extra/a.c
@@ -1,62 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    int n=0, m=0;
-    float A=0, B=0, C=0, D=0, E=0, h=0;
+int main() {
+    int n;
 
-    scanf("%d %d",&n,&m);
-    h=(n*m);
+    scanf("%d", &n);
 
-    char planeta [n][m];
-    for(int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-
-            scanf(" %c", &planeta[i][j]);
-
-            if(planeta[i][j]=='*'){
-                A++;
-            }
-
-            else if(planeta[i][j]=='~'){
-                B++;
-            }
-
-            else if(planeta[i][j]=='X'){
-                C++;
-            }
-
-            else if(planeta[i][j]=='^'){
-                D++;
-            }
-
-            else if(planeta[i][j]=='.'){
-                E++;
-            }
+    for (int i = 1; i <= n; i++){
+        if (i % 11 == 0 || i % 13 == 0 || i % 17 == 0){
+            printf("%d ", i);
         }
-
     }
-
-    if(C>1){
-        printf("Planeta hostil");
-    }
-
-    else if((A/h<=0.2) && B/h<=0.5){
-        printf("Planeta habitavel");
-    }
-    
-    else if(B/h>=0.85){
-        printf("Planeta aquatico");
-    }
-
-    else if(D>=0.6){
-        printf("Planeta desertico");
-    }
-
-    else if(A/h>=0.65){
-        printf("Planeta selvagem");
-    }
-
-    else{
-    printf("Planeta inospito");
-    }
+    return 0;
 }
