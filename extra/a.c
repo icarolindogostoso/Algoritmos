d61660a88d753bdbc5231322337b4be94236d662
Icaro Gabriel <icarogabrielpc05@gmail.com>
2024-11-11 22:03:18 -0300

    mensagem

--- extra/a.c
+++ extra/a.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int matriz [5][5] ={
+        {0,-1,0,0,0},
+        {0,0,0,0,-1},
+        {0,0,-1,0,0},
+        {0,0,0,0,0},
+        {0,-1,0,0,-1}
+    };
+    
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            if (matriz[i][j] == 0){
+                int contador = 0;
 
-    scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++){
-        if (i % 11 == 0 || i % 13 == 0 || i % 17 == 0){
-            printf("%d ", i);
+                for (int m = i-1; m <= i+1; m++){
+                    for (int n = j-1; n <= j+1; n++){
+                        if ((m < 0 || m > 4) || (n < 0 || n > 4)){
+                            continue;
+                        }
+                        if (matriz[m][n] == -1){
+                            contador++;
+                        }
+                    }
+                }
+                matriz[i][j] = contador;
+            }
         }
     }
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            printf("%02d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
