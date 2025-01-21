b5b5dc80b41e92607db01d75e23bd5d84ae06a42
ICARO GABRIEL PEREIRA CARVALHO <20241014040011@ifrn.local>
2025-01-21 07:41:00 -0300

    mensagem

--- Aula_08/ex01.c
+++ Aula_08/ex01.c
@@ -8,15 +8,71 @@ struct Node {
 
 struct Node* inicio = NULL;
 
-void inserir(int valor){
-    struct Node* novo = (struct Node*)malloc(sizeof(struct Node));
-    novo->valor = valor;
-    novo->proximo = inicio;
-    inicio = novo;
+void adicionar_elemento(int valor){
+    struct Node* novo = (struct Node*)malloc(sizeof(struct Node)); // cria-se um novo Node
+    novo->valor = valor; // o valor do novo Node é o valor dado no parametro da funcão
+    novo->proximo = inicio; // o proximo do novo é o Node inicio
+    inicio = novo; // o inicio agora é o novo
+
+    // antes -> [1] --> agora -> [2,1]
+    // complexidade O(1)
+    // a ordem dos elementos na lista é inversa à ordem de inserção (FILO / Pilha)
 }
 
-void remover (){
-    struct Node* temp = inicio;
+void adicionar_final(int valor){
+    struct Node *novo = (struct Node*)malloc(sizeof(struct Node)); // cria-se um novo Node
+    novo->valor = valor; // disse qual o valor do Node
+    novo->proximo = NULL; // já disse que o próximo valor do Node é NULL (agora tem que colocar ele na lista)
+
+    if (inicio == NULL){ // o inicio da lista é NULL? a lista está vazia?
+        inicio = novo; // se a lista está vazia é só inseir o Node na lista
+
+        // antes -> [] --> agora -> [1]
+        // complexidade O(1)
+    } else {
+        struct Node *atual = inicio; // cria-se um novo Node que recebe o inicio da lista
+        while (atual->proximo != NULL){ // o proximo não é NULL?
+            atual = atual->proximo; // se sim, atual recebe o proximo de atual
+        }
+        atual->proximo = novo; // se nao, o proximo do atual é o novo
+
+        // antes -> [1] --> agora -> [1,2]
+    }
+}
+
+void remover_elemento(int valor){
+    struct Node *atual = inicio;
+    struct Node *anterior = NULL;
+    while (atual != NULL){
+        if (atual->valor == valor){
+            if (anterior == NULL){
+                remover();
+            } else{
+                anterior->proximo = atual->proximo;
+            }
+            return;
+        }
+        anterior = atual;
+        atual = atual->proximo;
+    }
+}
+
+void remover_final(){
+    struct Node *atual = inicio;
+    struct Node *anterior = NULL;
+    while (atual->proximo != NULL){
+        anterior = atual;
+        atual = atual->proximo;
+    } if (anterior == NULL){
+        inicio = NULL;
+    } else {
+        anterior->proximo = NULL;
+    }
+    free(atual);
+}
+
+void remover_inicio(){
+    struct Node *temp = inicio;
     inicio = inicio->proximo;
     free(temp);
 }
@@ -31,16 +87,16 @@ int busca (int valor){
     }
 }
 
-void imprime (){
+void imprimir_lista (){
     struct Node* temp = inicio;
     while (temp != NULL){
-        printf("%d ", temp->valor);
+        printf("%d --> ", temp->valor);
         temp = temp->proximo;
     }
-    printf("\n");
+    printf(" NULL\n");
 }
 
-int tamanho (){
+int tamanho_da_lista (){
     int tam = 0;
     struct Node* temp = inicio;
     while (temp != NULL){
@@ -54,25 +110,58 @@ int vazia (){
     return inicio == NULL;
 }
 
+void imprimir_menu(){
+    printf("1. Adicionar elemento\n");
+    printf("2. Adicionar elemento no final\n");
+    printf("3. Remover elemento\n");
+    printf("4. Remover elemento no final\n");
+    printf("5. Remover elemento no inicio\n");
+    printf("6. Imprimir lista\n");
+    printf("7. Tamanho da lista\n");
+    printf("8. Sair\n");
+}
+
 int main (){
-    inserir(10);
-    inserir(20);
-    inserir(30);
-    inserir(40);
-    inserir(50);
-
-    imprime();
-
-    printf("Tamanho: %d\n", tamanho());
-
-    remover();
-    remover();
-
-    imprime();
-    printf("Tamanho: %d\n", tamanho());
-
-    printf("Busca: %d\n", busca(20));
-    printf("Busca: %d\n", busca(50));
-
+    //criar_lista()
+    int opcao;
+    int valor;
+    while(1){
+        imprimir_menu();
+        printf("Digite a opção: ");
+        scanf("%d", &opcao);
+        switch (opcao){
+            case 1:
+                printf("Digite o valor: ");
+                scanf("%d", &valor);
+                adicionar_elemento(valor);
+                break;
+            case 2:
+                printf("Digite o valor: ");
+                scanf("%d", &valor);
+                adicionar_final(valor);
+                break;
+            case 3:
+                printf("Digite o valor: ");
+                scanf("%d", &valor);
+                remover_elemento(valor);
+                break;
+            case 4:
+                remover_final();
+                break;
+            case 5:
+                remover_inicio();
+                break;
+            case 6:
+                imprimir_lista();
+                break;
+            case 7:
+                printf("Tamanho da lista: %d\n", tamanho_da_lista());
+                break;
+            case 8:
+                return 0;
+            default:
+                printf("Opção invalida\n");
+        }
+    }
     return 0;
 }
