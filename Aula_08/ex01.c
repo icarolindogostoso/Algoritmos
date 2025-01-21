3a8b583547c818d9164bdb21aee4fedf251a087f
ICARO GABRIEL PEREIRA CARVALHO <20241014040011@ifrn.local>
2025-01-21 07:46:39 -0300

    mensagem

--- Aula_08/ex01.c
+++ Aula_08/ex01.c
@@ -90,6 +90,7 @@ int busca (int valor){
 
 void imprimir_lista (){
     struct Node* temp = inicio;
+    printf("INICIO --> ")
     while (temp != NULL){
         printf("%d --> ", temp->valor);
         temp = temp->proximo;
